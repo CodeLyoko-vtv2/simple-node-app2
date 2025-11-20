6ec50897ee44f2fe50aa32ae8bce16538425f991
Nguyễn Vũ Huy <nguyenvuhuyx4@gmail.com>
2025-11-20 16:15:16 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+// Port do Render/Railway cung cấp
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World! This is my first deployed Node.js app!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
